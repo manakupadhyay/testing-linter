74a34daf5dda9d2eecee988a76dc6ebcfa51b6f5
Manak Upadhyay <manu742000@gmail.com>
2022-10-01 22:58:30 +0530

    test 3

--- helloWorld.js
+++ helloWorld.js
@@ -2,12 +2,13 @@ function helloWorld() {
     console.log('Hello World!')
 }
 
-function anotherFunction(){
+function anotherFunction() {
     let test;
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
         console.log(index);
     }
+    let a;
 }
 
 helloWorld()
