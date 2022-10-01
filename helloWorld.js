6a7832641e5915122ca4638b83902af223439c01
Manak Upadhyay <manu742000@gmail.com>
2022-10-01 22:24:11 +0530

    Add config files to test linter

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,13 @@
-function helloWorld(){
-    console.log("Hello World!");
+function helloWorld() {
+    console.log('Hello World!')
 }
+
+function anotherFunction(){
+    let test;
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(index);
+    }
+}
+
+helloWorld()
