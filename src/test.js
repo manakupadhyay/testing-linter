443c0d906dfeb40d226bcf20f0a102dbca409cea
Manak Upadhyay <manu742000@gmail.com>
2022-10-02 00:04:46 +0530

    test

--- src/test.js
+++ src/test.js
@@ -3,6 +3,7 @@ function helloWorld() {
 }
 
 function anotherFunction() {
+    let a
     const array = [1, 2, 3, 4, 5]
     for (let index = 0; index < array.length; index += 1) {
         const element = array[index]
