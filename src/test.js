c86fae40671d20d3b28aec839c10db1fd335a752
Manak Upadhyay <manu742000@gmail.com>
2022-10-01 23:12:18 +0530

    Add more files

---
+++ src/test.js
@@ -0,0 +1,14 @@
+function helloWorld() {
+    console.log('Hello World!')
+}
+
+function anotherFunction() {
+    let test;
+    for (let index = 0; index < array.length; index++){
+        const element = array[index];
+        console.log(index);
+    }
+    let a;
+}
+
+helloWorld()
